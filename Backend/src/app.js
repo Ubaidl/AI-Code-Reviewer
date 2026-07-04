14b8f400231a392c97b04bec31d9200b53392050
ubaid <loneubaid251@gmail.com>
2026-07-04 18:20:20 +0530

    Fix frontend deployment

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -17,8 +17,12 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 
+app.get("/", (req, res) => {
+  res.send("Backend is running!");
+})
 
 app.use("/api/auth",authrouter);
 app.use("/api/code", codeanalyzerouter);
+
 
 export default app;
