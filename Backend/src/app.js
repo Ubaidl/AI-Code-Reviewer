cfafa6612bd8518b9af237af239c760dab0c88a7
ubaid <loneubaid251@gmail.com>
2026-07-06 23:18:09 +0530

    Configure CORS for localhost and production

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -18,24 +18,13 @@ const app = express();
 
 
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  "http://localhost:5174",
-  "https://ai-code-reviewer-weld-tau.vercel.app"
-];
 
 app.use(cors({
-  origin: (origin, callback) => {
-    if (
-      !origin ||
-      allowedOrigins.includes(origin) ||
-      origin.endsWith(".vercel.app")
-    ) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:5174", // optional, if Vite sometimes uses 5174
+    "https://ai-code-reviewer-weld-tau.vercel.app"
+  ],
   credentials: true,
 }));
 app.use(express.json());
