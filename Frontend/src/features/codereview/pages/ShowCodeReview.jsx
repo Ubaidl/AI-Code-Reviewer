fce93dc59724f6543db5f21748acd869755d232c
ubaid <loneubaid251@gmail.com>
2026-07-08 10:31:10 +0530

    Redesign homepage with terminal/diff-inspired theme

--- Frontend/src/features/codereview/pages/ShowCodeReview.jsx
+++ Frontend/src/features/codereview/pages/ShowCodeReview.jsx
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import { Codecontext } from "../codeanalyzer.context";
-import "../style/result.css"
+// import "../style/result.css"
 
 const Result = () => {
   const { analysis } = useContext(Codecontext);
